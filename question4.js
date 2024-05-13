d284162ccf3db5b2d9939669a3a07c6761b3ebd5
bushra-n <160867479+bushra-n@users.noreply.github.com>
2024-05-13 17:26:08 +0500

    Add files via upload

---
+++ question4.js
@@ -0,0 +1,13 @@
+// question no.4   Famous quote.find aquote from a person you admire .print the quote and its author name.your output should 
+// have author name and quotation mark.
+var quote = "I do not believe in taking the right decision, I take a decision and make it right.";
+var author = "Quaid e azam";
+console.log("".concat(author, " once said,\"").concat(quote, "\""));
+// question no.5    Famous quote2; repeat exercise 
+var famous_person = "Quaid-e-azam";
+var message = "".concat(famous_person, " once said,\"").concat(quote, "\"");
+console.log(message);
+// question no.6     (My output dont showing ""around john doe name)
+var personname = "\t\n  John Doe  \n\t";
+console.log("Original:", personname);
+console.log("Stripped:", personname.trim());
---
+++ question4.ts
@@ -0,0 +1,20 @@
+// question no.4   Famous quote.find aquote from a person you admire .print the quote and its author name.your output should 
+// have author name and quotation mark.
+
+
+let quote:string="I do not believe in taking the right decision, I take a decision and make it right.";
+let author:string="Quaid e azam";
+console.log(`${author} once said,"${quote}"`) ;
+
+
+// question no.5    Famous quote2; repeat exercise 
+let famous_person:string="Quaid-e-azam"
+
+let message:string=`${famous_person} once said,"${quote}"`;
+console.log(message)
+
+// question no.6     (My output dont showing ""around john doe name)
+
+let personname: string = "\t\n  John Doe  \n\t";
+console.log("Original:",  personname);
+console.log("Stripped:" , personname.trim());
